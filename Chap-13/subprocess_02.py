0a951d5de67cb7a516074d50f8200d4db78f353c
Yogi Kapoor <yogi_kapoor@hotmail.com>
2021-10-22 15:31:57 -0400

    os py

--- Chap-13/subprocess_02.py
+++ Chap-13/subprocess_02.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import subprocess
 
-p1 = subprocess.run(['ls','-ltr'], shell=True, capture_output=True)
+p1 = subprocess.run('dir', shell=True, capture_output=True)
 print(p1)
 print(p1.args, p1.returncode)
-print(p1.stdout) # to capture the out you need to use capture_output
+print(p1.stdout.decode()) # to capture the out you need to use capture_output
 
 # p2 = subprocess.run()
