8c9d195012fd6fec3f1611fd278a766f45b0d6a7
Yogi Kapoor <yogi_kapoor@hotmail.com>
2021-10-22 15:31:34 -0400

    few more additions

---
+++ Chap-13/os_module.py
@@ -0,0 +1,15 @@
+import os
+
+print(os.getcwd())
+os.chdir('$HOME')
+print(os.getcwd())
+print(os.listdir())
+print(os.listdir("/home"))
+os.mkdir("test")
+os.removedirs("test")
+print(os.listdir())
+os.makedirs("abc/def")
+os.rename("abc","abc123")
+print(os.environ.get('HOME'))
+#os.path.join()
+os.path.exists('HOME')
